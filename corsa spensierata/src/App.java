ba674b1650a4aa795c388b346b6de592a01df9e7
Di Giacomo Daniel <daniel.digiacomo98@gmail.com>
2024-12-20 16:13:04 +0100

    Step 2 fatto

--- corsa spensierata/src/App.java
+++ corsa spensierata/src/App.java
@@ -6,13 +6,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Dichiarazione scanner
         Scanner sc = new Scanner(System.in);
-        
+
         // Dimensione percorso
         System.out.println("Scegli quanti ostacoli vuoi");
         int lunghezzaPercorso = sc.nextInt();
-        
+
+        // Contatore array
+        int contatoreArray = 0;
+
         // Ostacoli
         String ostacoli[] = { "strada", "buca", "muro", "piscina" };
+
+        // Azioni da fare
+        String azioni[] = { "corri", "salta", "arrampicati", "nuota" };
 
         // Scelta con numeri casuali
         Random scelta = new Random();
@@ -30,7 +36,38 @@ public class App {
         for (int i = 0; i < percorsoGenerato.size(); i++) {
             System.out.print(percorsoGenerato.get(i) + " ");
         }
+        System.out.println();
+        int turno = 0;
 
+        // Esce fuori dal while una volta finito tutti gli ostacoli
+        while (lunghezzaPercorso != contatoreArray) {
+            // Mostro il turno
+            System.out.println("Turno n" + (turno + 1));
+            // Creo un azione casuale
+            int sceltaAzioni = scelta.nextInt(azioni.length);
+            String azionepresa = azioni[sceltaAzioni];
+            // Mostro l'azione
+            System.out.println("l'azione presa per " + percorsoGenerato.get(contatoreArray) + " è " + azionepresa);
 
+            // Controllo se l'azione è giusta
+            if (percorsoGenerato.get(contatoreArray).equals("strada") && azionepresa.equals("corri")) {
+                System.out.println("Il giocatore corre lungo la strada e supera l'ostacolo");
+                contatoreArray++;
+            } else if (percorsoGenerato.get(contatoreArray).equals("buca") && azionepresa.equals("salta")) {
+                System.out.println("Il giocatore salta il buco e supera l'ostacolo");
+                contatoreArray++;
+            } else if (percorsoGenerato.get(contatoreArray).equals("muro") && azionepresa.equals("arrampicati")) {
+                System.out.println("Il giocatore si arrampica sul muro e supera l'ostacolo");
+                contatoreArray++;
+            } else if (percorsoGenerato.get(contatoreArray).equals("piscina") && azionepresa.equals("nuota")) {
+                System.out.println("Il giocatore nuota nella piscina e supera l'ostacolo");
+                contatoreArray++;
+            } else {
+                System.out.println("Azione sbagliata, prova di nuovo!");
+            }
+            // Aumento il conteggio del turno
+            turno++;
+        }
+        sc.close();
     }
 }
